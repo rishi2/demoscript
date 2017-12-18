690bce1bc621a3bd79970d2c52469e5b58f9c3b5
rishi2 <hrishikesh.srivastava@renovite.com>
2017-12-18 10:38:21 +0530

    [initial commit.]

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # demoscript
 script to execute all services in different terminal tabs
+
+1. Update PATH for all services from your local machine in to start.sh
+2. set is_build_required to false if no build required.
+3. execute `./start.sh' from terminal.
+
+
+4. There are 30 sec gap to start some services so, you have to login do gauth within 30 secs once you get gcloud auth page and it will return back to terminal.
+5. Once conversation services is up and ngrok URL generated, within 30 secs have to update 'header.ejs' file with conversation's ngrok url in 'retailerwebdemo' services.
+
+6. Once `retailerwebdemo` service is up and ngrok url created,need to whitelist it for both services: `conversation` and `retailerwebdemo`
+7. add Webhooks into facebook developer page.
---
+++ start.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+#rishi's script
+
+is_build_required=true
+
+PATH_RETAILER=/Users/renovite/westfield/wrsinc/retailer/retailer
+
+PATH_ORDER=/Users/renovite/westfield/wrsinc/order/order
+
+PATH_MESSAGING=/Users/renovite/westfield/wrsinc/messaging/messaging
+
+PATH_RECEIPT=/Users/renovite/westfield/wrsinc/receipt/receipt
+
+PATH_MEMBERPREFERENCES=/Users/renovite/westfield/wrsinc/memberpreferences/memberpreferences
+
+PATH_TEMPLATE=/Users/renovite/westfield/wrsinc/template/template
+
+PATH_REDIS=/Users/renovite/westfield
+
+PATH_CONVERSATION=/Users/renovite/westfield/wrsinc/conversation/conversation
+
+PATH_RETAILERWEBDEMO=/Users/renovite/westfield/wrsinc/retailerwebdemo/retailerwebdemo/src
+
+PATH_EMAIL=/Users/renovite/westfield/wrsinc/email
+
+PATH_IDENTITY=/Users/renovite/go/src/github.com/wrsinc/identity
+
+PATH_DATABUNKER=/Users/renovite/go/src/github.com/wrsinc/databunker
+
+PATH_NGROK=/Users/renovite/westfield/downloads
+
+
+if [ "$is_build_required" = true ] ; then
+	JAVA_BUILD_COMMAND="mvn clean install;"
+	JAVA_BUILD_COMMAND_SKIPTEST="mvn clean install -DskipTests;"
+	NPM_BUILD_COMMAND="npm install;"
+	GO_BUILD_COMMAND="go build;"
+fi
+
+
+function new_tab() {
+  TAB_NAME=$1
+  COMMAND=$2
+  osascript \
+    -e "tell application \"Terminal\"" \
+    -e "tell application \"System Events\" to keystroke \"t\" using {command down}" \
+    -e "do script \"printf '\\\e]1;$TAB_NAME\\\a'; $COMMAND\" in front window" \
+    -e "end tell" > /dev/null
+}
+
+echo "starting retailer service in new tab..."
+new_tab "retailer" "cd $PATH_RETAILER; $JAVA_BUILD_COMMAND mvn spring-boot:run"
+
+
+echo "starting Order service in new tab..."
+new_tab "order" "cd $PATH_ORDER; $JAVA_BUILD_COMMAND mvn spring-boot:run"
+
+
+echo "starting Receipt service in new tab..."
+new_tab "receipt" "cd $PATH_RECEIPT; $JAVA_BUILD_COMMAND mvn spring-boot:run"
+
+
+echo "starting Messaging service in new tab..."
+new_tab "messaging" "cd $PATH_MESSAGING; $JAVA_BUILD_COMMAND_SKIPTEST mvn -Drun.profiles=development spring-boot:run"
+
+
+echo "starting Memberpreferences service in new tab..."
+new_tab "memberpreferences" "cd $PATH_MEMBERPREFERENCES; $JAVA_BUILD_COMMAND mvn spring-boot:run"
+
+
+echo "starting Template service in new tab..."
+new_tab "template" "cd $PATH_TEMPLATE; $JAVA_BUILD_COMMAND_SKIPTEST mvn spring-boot:run -Dspring.profiles.active=development,emulator"
+
+
+echo "starting redis service in new tab..."
+new_tab "redis" "cd $PATH_REDIS; redis-server"
+
+
+echo "starting email service in new tab..."
+new_tab "email" "cd $PATH_EMAIL;  $JAVA_BUILD_COMMAND mvn spring-boot:run"
+
+
+sleep 30s
+
+echo "starting databunker service in new tab..."
+new_tab "databunker" "cd $PATH_DATABUNKER; $GO_BUILD_COMMAND ./server.sh"
+
+echo "auth identity service in new tab..."
+new_tab "gauth" "cd $PATH_IDENTITY; gcloud beta auth login"
+
+sleep 30s #wait for 30 seconds to google auth and execute command afterward
+
+echo "starting pubsub emulator in new tab..."
+new_tab "pubsub" "cd $PATH_IDENTITY; gcloud beta emulators pubsub start"
+
+echo "starting datastore emulator in new tab..."
+new_tab "datastore" "cd $PATH_IDENTITY; gcloud beta emulators datastore start"
+
+
+echo "starting identity service in new tab..."
+
+new_tab "identity" "cd $PATH_IDENTITY; $IDENTITY_BUILD_COMMAND go run main.go"
+
+
+echo "starting conversation service in new tab..."
+new_tab "conversation" "cd $PATH_CONVERSATION; $NPM_BUILD_COMMAND ./start.sh"
+
+sleep 40s #wait for 30 seconds to before creating ngrok url for conversation
+
+echo "starting ngrok url for conversation service in new tab..."
+new_tab "ngrok" "cd $PATH_NGROK; ./ngrok http 9090"
+
+
+echo "update header.ejs with conversation ngrok url before running retailerwebdemo service..."
+
+
+sleep 30s #wait for 30 seconds to before creating ngrok url for retailerwebdemo
+
+
+echo "starting retailerwebdemo service in new tab..."
+new_tab "retailerwebdemo" "cd $PATH_RETAILERWEBDEMO; $NPM_BUILD_COMMAND node app.js"
+
+
+echo "starting ngrok url for retailerwebdemo service in new tab..."
+new_tab "ngrok" "cd $PATH_NGROK; ./ngrok http 8080"
+
+sleep 30s
+
+echo "All services started successfully... "
